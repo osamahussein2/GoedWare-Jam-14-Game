bdf3ceffe3c86be8af29eedb6d2d7baf812a3726
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-29 10:49:37 -0400

    Changed the function in main.cpp to accomdate for different subsystem options

--- GoedWare Jam 14 Project/src/Main.cpp
+++ GoedWare Jam 14 Project/src/Main.cpp
@@ -1,10 +1,12 @@
 #include "Engine.h"
 #include <Windows.h>
 
+#ifdef NDEBUG
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+#else
 int main()
+#endif
 {
-	FreeConsole();
-
 	Engine::Instance()->RunEngine();
 
 	return 0;
